9d110ea2e7f40594a14056259fc5db850f3f9d94
george kamau <flexkamau646@gmail.com>
2020-02-14 08:44:28 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
 function getDay(name){
     var month = parseInt(document.getElementById("month").value);
     var date = parseInt(document.getElementById("date").value);
     var year = document.getElementById("year").value;
     var century = year.slice(0, 2);
     var years = year.slice(2, 4);
-
     if (month === "" || month < 1 || month > 12 || date === "" || date < 0 || date > 31 || year === "" || year.length > 4 || name === ""){
         alert("Confirm that you have entered the correct information!")
     }else{
@@ -13,10 +11,8 @@ function getDay(name){
         return day
     }
 }
-
 function getGender(){
-    var radio = document.getElementsByName("gender"); 
-
+    var radio = document.getElementsByName("gender");
     for (var i = 0; i < radio.length; i++){
         if (radio[i].checked){
             var gender = radio[i].value;
@@ -24,13 +20,10 @@ function getGender(){
         }
     }
 }
-
 function getName(){
     var name =  document.getElementById("name").value;
-
     var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
     if (getGender() === "male"){
         document.getElementById("post").innerHTML = `Hey ${name}! if you were born in Ghana your name would be ${male[getDay(name)]}`;
     }else if(getGender() === "female"){
